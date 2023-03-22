30a2da4b46efd34ca3656cf11053cbacb56e0477
Ezz690 <abouelez10@gmail.com>
2023-03-22 17:17:11 +0200

    add migration files

---
+++ Back/database/migrations/2023_03_22_140807_create_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->string('thumbnail');
+            $table->decimal('price',10,2);
+            $table->integer('total_enrollments')->default(0);
+            $table->double('total_hours');
+            $table->double('rating')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
